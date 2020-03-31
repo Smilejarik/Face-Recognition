d7f8af6e8c6efb7939f78d48c80b25d3f22af860
Smilejarik <39190514+Smilejarik@users.noreply.github.com>
2020-03-31 11:36:28 +0200

    update

--- 01_face_recogn.py
+++ 01_face_recogn.py
@@ -9,8 +9,9 @@ print(cuda.get_num_devices())
 
 print("Dlib CUDA: {}".format(dlib.DLIB_USE_CUDA))
 
-KNOWN_FACES_DIR = "D:/Downloads/ML_Training/face_recognition_training/known_faces"
-UNKNOWN_FACES_DIR = "D:/Downloads/ML_Training/face_recognition_training/unknown_faces"
+KNOWN_FACES_DIR = "D:\\Google Drive\\Colab Notebooks\\OpenCV_Face_Detect\\known_faces"
+UNKNOWN_FACES_DIR = "D:\\Google Drive\\Colab Notebooks\\OpenCV_Face_Detect\\unknown_faces"
+DETECTED_FACES_DIR = "D:\\Google Drive\\Colab Notebooks\\OpenCV_Face_Detect\\detected_images"
 TOLERANCE = 0.6
 FRAME_THICKNESS = 2
 FONT_THICKNESS = 2
@@ -25,7 +26,10 @@ for name in os.listdir(KNOWN_FACES_DIR):
     for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
         print(f"Filename: {filename}")
         image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
-        encoding = face_recognition.face_encodings(image)[0]
+        try:
+            encoding = face_recognition.face_encodings(image)[0]
+        except Exception:
+            print("! Not detected")
         #print(f"Encoding: {encoding}")
         known_faces.append(encoding)
         known_names.append(name)
@@ -35,10 +39,11 @@ print("processing unknown faces...")
 for filename in os.listdir(UNKNOWN_FACES_DIR):
     print("Filename is: {}".format(filename))
     image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
-    resize_to_w = 1200
+    resize_to_w = 1000
     img_w = image.shape[1]
     img_h = image.shape[0]
-    image = cv2.resize(image, (resize_to_w, int(resize_to_w*img_h/img_w)))
+    if img_w > resize_to_w:
+        image = cv2.resize(image, (resize_to_w, int(resize_to_w*img_h/img_w)))
     locations = face_recognition.face_locations(image, model=MODEL)
     encodings = face_recognition.face_encodings(image, locations)
 
@@ -63,7 +68,10 @@ for filename in os.listdir(UNKNOWN_FACES_DIR):
             bottom_right = (face_location[1], face_location[2])
             #cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
             cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, \
-                        0.5, (200, 200, 200), FONT_THICKNESS)
+                        0.5, (50, 255, 50), FONT_THICKNESS)
     cv2.imshow(filename, image)
+    cv2.imwrite(f"{DETECTED_FACES_DIR}/{filename}", image)
     cv2.waitKey(3000)
     cv2.destroyWindow(filename)
+
+print("Finished")
